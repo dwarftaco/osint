9571c1b235dc87d0cc59a9189c4449a3e4fb5b62
dwarftaco <thedwarftaco@gmail.com>
2024-12-26 23:32:19 +1100

    Added CCTV grid

--- index.js
+++ index.js
@@ -48,9 +48,33 @@ function reloadIFrame() {
     document.getElementById("dashboard").src = "https://docs.google.com/document/d/e/2PACX-1vRRCU4PUvB_wtGfXlDvmlJ1jgXgvdZTruy_4eGW70i4a3yixU4XIY9aZxEz8FJaU5-OoJddGJIO91xW/pub?embedded=true";
     document.getElementById("map").src = "https://www.google.com/maps/d/u/6/embed?mid=1kWBQ9SNwwDjUrEqiFN1LQ6r5q6pWpGw&ehbc=2E312F&noprof=1";
     document.getElementById("traffic").src = "https://www.google.com/maps/vt/data=XHPXLaEuKujieiEd8RwyvMDhzojV5_gjWQSbPscZ_9NX2yhhyRiMCpfJoDhfSpm9eD6RZPEjKQbe1ppLMbogrTVPaicCJiBPBHmHz766KpQ3_mtYMRmd-vxBMjBbWLuO6sG_CNtcqwIHw7Dt0UNQGQGzvFshZg5HVLXmyoWyYsdRE0_vY1mezqVGFLn80Lk2KefwGc1a2Rh3tcFqNPd6p5lbeJZ6_R1qYFuQx0STM_4sx8m1u4IfFLYq1sCSj4XYgDSgS_zcwpgzDO7w5A";
-    document.getElementById("cctv").src = "https://wxyzwebcams.com/network/joe.php?url=https://cdn3.wowza.com/5/REFkVjRKS0dzbTYr/live/group1Delayed_360p/playlist.m3u8";
     document.getElementById("misc").src = "https://docs.google.com/document/d/e/2PACX-1vQB3aoK1ks-cDKkrYvW1mSHQBZvb806UR7yJNOkw8M39ZOahSprImaA_CyXdc7Ycy4uQj9ClsSa9nKg/pub?embedded=true";
 }
+
+
+var x = setInterval(function () {
+    Array.from(document.getElementsByClassName("cctv-reload")).forEach(
+        function (element, index, array) {
+            element.load()
+        }
+    );
+}, 30000);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    Array.from(document.getElementsByClassName("cctv-reload")).forEach(
+        function (element, index, array) {
+            element.addEventListener('loadstart', function () {
+                element.setAttribute("class", "cctv-reload loading");
+                console.log("loading");
+            });
+            element.addEventListener('canplay', function () {
+                element.setAttribute("class", "cctv-reload");
+            });
+        }
+    );
+});
+
 
 function openModal(block) {
     document.body.setAttribute("class", "modal-open");
