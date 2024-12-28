6ac20b402fadc9587f0d6ad506482c7d07d6397c
dwarftaco <thedwarftaco@gmail.com>
2024-12-28 18:22:48 +1100

    Updated OSINT to Wasilla

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var iframeMap = {
     "Misc": "https://docs.google.com/document/d/e/2PACX-1vQB3aoK1ks-cDKkrYvW1mSHQBZvb806UR7yJNOkw8M39ZOahSprImaA_CyXdc7Ycy4uQj9ClsSa9nKg/pub?embedded=true"
 }
 
-var countDownDate = new Date("Dec 27, 2024 17:00:00 GMT-5").getTime();
+var countDownDate = new Date("Jan 31, 2025 11:59:59 GMT-9").getTime();
 
 var x = setInterval(function () {
 
@@ -29,11 +29,11 @@ var x = setInterval(function () {
         countdownString += days + "d " + hours + "h " + minutes + "m " + seconds + "s";
     }
 
-    countdownString += " until Live Drop"
+    countdownString += " to mail <a href='https://docs.google.com/document/d/1lGUMbrQVQfq_x88HOqB9pcCYU-D4mMvZun44ImelmkA/edit?tab=t.0'>Agent Reactivation Form</a>"
 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = "Live Drop is In Progress";
+        document.getElementById("countdown").innerHTML = "You snooze, you lose";
     } else {
         document.getElementById("countdown").innerHTML = countdownString;
     }
@@ -41,7 +41,7 @@ var x = setInterval(function () {
 }, 1000);
 
 let options = {
-    timeZone: 'EST',
+    timeZone: 'America/Anchorage',
     year: 'numeric',
     month: 'numeric',
     day: 'numeric',
@@ -52,7 +52,7 @@ let options = {
     formatter = new Intl.DateTimeFormat([], options);
 
 var x = setInterval(function () {
-    document.getElementById("datetime").innerHTML = "Montreal (GMT-5)<br>" + formatter.format(new Date());
+    document.getElementById("datetime").innerHTML = "Wasilla (GMT-9)<br>" + formatter.format(new Date());
 }, 1000);
 
 
@@ -63,37 +63,23 @@ function reloadIFrame() {
     document.getElementById("misc").src = "https://docs.google.com/document/d/e/2PACX-1vQB3aoK1ks-cDKkrYvW1mSHQBZvb806UR7yJNOkw8M39ZOahSprImaA_CyXdc7Ycy4uQj9ClsSa9nKg/pub?embedded=true";
 }
 
+var cameraIds = ['oq0qklzzaxq', 'mk2bn53xxau', 'ni3xljjihcx', 'hl2hpbwdx2d', 'q5lqrl02utl'];
 
-var cameraIds = ['3814', '3410', '3411', '3412', '3413', '3414', '3415', '3416', '3417', '3419', '3420', '3421', '3422', '3423', '3369', '3815', '3816', '3319', '3322', '3324', '3325', '3326', '3327', '3328', '3329', '3330', '3331', '3332', '3333', '3334', '3335', '3336', '3337', '3338', '3339', '3342', '3343', '3344', '3345', '3347', '3348', '3349', '3350', '3351', '3352', '3353', '3361', '3362', '3363', '3364', '3365', '3366', '3367', '3370', '3373', '3374', '3375', '3376', '3377', '3378', '3379', '3380', '3381', '3382', '3383', '3385', '3386', '3387', '3388', '3389', '3390', '3391', '3393', '3394', '3395', '3397', '3398', '3399', '3400', '3371', '3372', '3426', '3427', '3428', '3429', '3430', '3431', '3432', '3433', '3434', '3435', '3436', '3438', '3439', '3440', '3441', '3443', '3444', '3446', '3451', '3452', '3418', '3341', '3360', '3392', '3442', '3340', '3445', '3598', '3599', '3600', '3346', '3716', '3717', '3718', '3719', '3720', '3721', '3722', '3723', '3724', '3726', '3727', '3728', '3729', '3730', '3731', '3732', '3733', '3734', '3735', '3736', '3737', '3738', '3739', '3368', '3384', '3437', '3396', '3323', '3402', '3798', '3799', '3800', '3801', '3802', '3803', '3804', '3808', '3809', '3810', '3811', '3805', '3424', '3425', '3401', '3403', '3404', '3405', '3406', '3407', '3408', '3409', '3828', '3829', '3830', '3853', '3866', '3900', '3901', '3855', '3875', '3899', '3917', '3918', '3919', '3854', '3891', '3892', '3894', '3895', '3927', '3935', '3959', '3960', '3961', '3962', '3963', '4018', '4023', '4019', '4020', '4035', '4021', '4022', '4024'];
-
-
-let myArray = [];
-
-function randomNumbers(number, max) {
-    var arr = [];
-    while (arr.length < number) {
-        var r = Math.floor(Math.random() * max) + 1;
-        if (arr.indexOf(r) === -1) arr.push(r);
-    }
-
-    return arr;
-}
-
-function randomiseCctv () {
+function reloadCctv () {
     let cctvVideos = Array.from(document.getElementsByClassName("cctv-reload"));
-    var cctvIds = randomNumbers(cctvVideos.length, cameraIds.length);
+
+    let sec = Math.floor(Date.now() / 1000);
+
     cctvVideos.forEach(
         function (element, index, array) {
-            element.src = "https://www.quebec511.info/diffusion/fr/camera/camera.ashx?format=mp4&id=" + cameraIds[cctvIds[index]];
+            element.src = "https://511.alaska.gov/map/Cctv/" + cameraIds[index] + "--1?t=" + sec;
         }
     );
 }
 
 let proxyUrl = 'https://api.allorigins.win/get?url='; // Public CORS proxy
 let rssFeeds = {
-    //'Montreal Gazette': 'http://www.montrealgazette.com/montrealgazette/blogs/metropolitannews/rss.aspx',
     'CTV': 'https://montreal.ctvnews.ca/rss/ctv-news-montreal-1.822366',
-    //'CityNews': 'https://montreal.citynews.ca/feed/'
 }
 var rssFeedItems = [];
 
@@ -151,21 +137,21 @@ function displayFeedItems(feedItems) {
         .join('');
 }
 
-var x = setInterval(randomiseCctv, 30000);
+var x = setInterval(reloadCctv, 5000);
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    randomiseCctv();
+    reloadCctv();
   
-    Object.entries(rssFeeds).forEach(([rssName, rssUrl]) => {
-        fetchRSSWithProxy(proxyUrl, rssUrl).then((rssText) => {
-            const xmlDoc = parseRSS(rssText);
-            const feedItems = extractFeedItems(xmlDoc);
-            // add to rssFeedItems sorted by pubDate:
-            rssFeedItems = rssFeedItems.concat(feedItems);
-            rssFeedItems.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
-            displayFeedItems(rssFeedItems);
-        });
-    });
+    // Object.entries(rssFeeds).forEach(([rssName, rssUrl]) => {
+    //     fetchRSSWithProxy(proxyUrl, rssUrl).then((rssText) => {
+    //         const xmlDoc = parseRSS(rssText);
+    //         const feedItems = extractFeedItems(xmlDoc);
+    //         // add to rssFeedItems sorted by pubDate:
+    //         rssFeedItems = rssFeedItems.concat(feedItems);
+    //         rssFeedItems.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+    //         displayFeedItems(rssFeedItems);
+    //     });
+    // });
 });
 
 function openModal(block) {
